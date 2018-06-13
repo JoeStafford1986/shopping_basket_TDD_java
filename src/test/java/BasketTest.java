345aa8bb575ae8c0547ded74061bb8d76fb7c59a
Joe Stafford <joestafford1986@gmail.com>
2018-06-13 19:00:47 +0100

    added more tests to add method

--- src/test/java/BasketTest.java
+++ src/test/java/BasketTest.java
@@ -5,10 +5,15 @@ import static org.junit.Assert.assertEquals;
 
 public class BasketTest {
     private Basket basket;
+    private Item item1;
+    private Item item2;
 
     @Before
     public void before() {
         basket = new Basket();
+
+        item1 = new Item(1);
+        item2 = new Item(2);
     }
 
     @Test
@@ -18,8 +23,21 @@ public class BasketTest {
 
     @Test
     public void canAddItem() {
-        Item item = new Item(1);
-        basket.addItem(item);
+        basket.addItem(item1);
         assertEquals(1, basket.getItemsCount());
+    }
+
+    @Test
+    public void canAddMultipleItemsSame() {
+        basket.addItem(item1);
+        basket.addItem(item1);
+        assertEquals(2, basket.getItemsCount());
+    }
+
+    @Test
+    public void canAddMultipleItemsDifferent() {
+        basket.addItem(item1);
+        basket.addItem(item2);
+        assertEquals(2, basket.getItemsCount());
     }
 }
