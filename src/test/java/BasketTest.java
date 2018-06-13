86a00ae17e3a62c705574cdf4b319615287d1008
Joe Stafford <joestafford1986@gmail.com>
2018-06-13 20:15:32 +0100

    added tests for 10% discount

--- src/test/java/BasketTest.java
+++ src/test/java/BasketTest.java
@@ -7,13 +7,15 @@ public class BasketTest {
     private Basket basket;
     private Item item1;
     private Item item2;
+    private Item item3;
 
     @Before
     public void before() {
         basket = new Basket();
 
-        item1 = new Item(1);
-        item2 = new Item(2);
+        item1 = new Item(1.00);
+        item2 = new Item(2.00);
+        item3 = new Item(21.00);
     }
 
     @Test
@@ -105,9 +107,23 @@ public class BasketTest {
 
     @Test
     public void canDiscountBasketWhenTotalHighEnough() {
-        Item item3 = new Item(21.00);
         basket.addItem(item3);
-        assertEquals(17.90, basket.calculateTotalCost(), 1.00);
+        assertEquals(17.90, basket.calculateTotalCost(), 0.01);
+    }
+
+    @Test
+    public void canDiscountBasketWhenTotalHighEnoughMultipleItems() {
+        basket.addItem(item1);
+        basket.addItem(item3);
+        assertEquals(19.80, basket.calculateTotalCost(), 0.01);
+    }
+
+    @Test
+    public void canDiscountBasketWhenTotalHighEnoughMultipleLargeItems() {
+        Item item4 = new Item(500.00);
+        basket.addItem(item3);
+        basket.addItem(item4);
+        assertEquals(468.90, basket.calculateTotalCost(), 0.01);
     }
 
     @Test
