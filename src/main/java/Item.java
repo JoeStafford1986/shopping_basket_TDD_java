604d2390d5a59945128f0dd00b1b1b20bd4e0053
Joe Stafford <joestafford1986@gmail.com>
2018-06-14 06:36:42 +0100

    added boolean to item

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,11 +1,17 @@
 public class Item {
     private double cost;
+    private boolean checkIfCalculated;
 
     public Item(double cost) {
         this.cost = cost;
+        this.checkIfCalculated = false;
     }
 
     public double getCost() {
         return this.cost;
+    }
+
+    public boolean checkIfCalculated() {
+        return this.checkIfCalculated;
     }
 }
--- src/test/java/ItemTest.java
+++ src/test/java/ItemTest.java
@@ -1,12 +1,23 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class ItemTest {
+    private Item item;
+
+    @Before
+    public void before() {
+        item = new Item(1.00);
+    }
 
     @Test
     public void canGetCost() {
-        Item item = new Item(1.00);
         assertEquals(1.00, item.getCost(), 0.01);
+    }
+
+    @Test
+    public void canCheckIfCalculatedStartsFalse() {
+        assertEquals(false, item.checkIfCalculated());
     }
 }
