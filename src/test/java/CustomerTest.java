7d5a8174f50314769941e2658dea8768275ea2c2
Joe Stafford <joestafford1986@gmail.com>
2018-06-14 10:01:34 +0100

    added customer test class

---
+++ src/test/java/CustomerTest.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CustomerTest {
+    private Customer customer;
+
+    @Before
+    public void before() {
+        customer = new Customer();
+    }
+
+    @Test
+    public void canCheckLoyalty() {
+        assertEquals(false, customer.checkLoyalty());
+    }
+}
