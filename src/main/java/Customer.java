4810184a2d07e3654bf925c4bd0f68ef5ebf4667
Joe Stafford <joestafford1986@gmail.com>
2018-06-14 10:03:16 +0100

    added customer class, can get loyalty

---
+++ src/main/java/Customer.java
@@ -0,0 +1,11 @@
+public class Customer {
+    private boolean loyal;
+
+    public Customer() {
+        this.loyal = false;
+    }
+
+    public boolean checkLoyalty() {
+        return this.loyal;
+    }
+}
